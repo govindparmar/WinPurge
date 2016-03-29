aac449101cf4e03b6d950295358557ed14eabbe3
Govind Parmar <0.govindparmar@gmail.com>
2016-03-29 07:18:39 -0600

    Added message crackers

--- wpurge/purge.c
+++ wpurge/purge.c
@@ -1,4 +1,5 @@
 #include <Windows.h>
+#include <WindowsX.h>
 #include <Psapi.h>
 #include <TlHelp32.h>
 #include <strsafe.h>
@@ -53,43 +54,53 @@ BOOL CALLBACK EnumChildProc(HWND hWnd, LPARAM lParam)
 	return TRUE;
 }
 
+VOID WINAPI OnClose(HWND hWnd)
+{
+	DestroyWindow(hWnd);
+}
+
+VOID WINAPI OnDestroy(HWND hWnd)
+{
+	PostQuitMessage(0);
+}
+
+VOID WINAPI OnCommand(HWND hWnd, INT nID, HWND hwSource, UINT uNotify)
+{
+	HWND hButton = FindWindowEx(hWnd, NULL, L"BUTTON", NULL);
+	if (hButton == hwSource)
+	{
+		PROCESSENTRY32 entry;
+		HANDLE hSnapShot, hProcess;
+		BOOL hRes;
+		WCHAR fPath[MAX_PATH], username[32];
+		entry.dwSize = sizeof(PROCESSENTRY32);
+		hSnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPALL, 0);
+		hRes = Process32First(hSnapShot, &entry);
+		while (hRes)
+		{
+			if (wcscmp(entry.szExeFile, L"explorer.exe") == 0)
+			{
+				hProcess = OpenProcess(PROCESS_TERMINATE, 0, (DWORD)entry.th32ProcessID);
+				TerminateProcess(hProcess, 0);
+				CloseHandle(hProcess);
+				GetEnvironmentVariable(L"username", username, 32);
+				StringCchPrintf(fPath, MAX_PATH, L"C:\\Users\\%s\\AppData\\Local\\IconCache.db", username);
+				DeleteFile(fPath);
+				// NOTE: No break after successful find because there may be more than one instance of explorer.exe running (folder windows etc.); only safe method is to kill all
+			}
+			hRes = Process32Next(hSnapShot, &entry);
+		}
+	}
+}
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)
 {
 	HWND hButton = FindWindowEx(hWnd, NULL, L"BUTTON", NULL);
 	switch (Msg)
 	{
-	case WM_CLOSE:
-		DestroyWindow(hWnd);
-		break;
-	case WM_COMMAND:
-		if ((HWND)lParam == hButton)
-		{
-			PROCESSENTRY32 entry;
-			HANDLE hSnapShot, hProcess;
-			BOOL hRes;
-			WCHAR fPath[MAX_PATH], username[32];
-			entry.dwSize = sizeof(PROCESSENTRY32);
-			hSnapShot = CreateToolhelp32Snapshot(TH32CS_SNAPALL, 0);
-			hRes = Process32First(hSnapShot, &entry);
-			while (hRes)
-			{
-				if (wcscmp(entry.szExeFile, L"explorer.exe") == 0)
-				{
-					hProcess = OpenProcess(PROCESS_TERMINATE, 0, (DWORD)entry.th32ProcessID);
-					TerminateProcess(hProcess, 0);
-					CloseHandle(hProcess);
-					GetEnvironmentVariable(L"username", username, 32);
-					StringCchPrintf(fPath, MAX_PATH, L"C:\\Users\\%s\\AppData\\Local\\IconCache.db", username);
-					DeleteFile(fPath);
-					// NOTE: No break after successful find because there may be more than one instance of explorer.exe running (folder windows etc.)
-				}
-				hRes = Process32Next(hSnapShot, &entry);
-			}
-		}
-		break;
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
+		HANDLE_MSG(hWnd, WM_CLOSE, OnClose);
+		HANDLE_MSG(hWnd, WM_DESTROY, OnDestroy);
+		HANDLE_MSG(hWnd, WM_COMMAND, OnCommand);
 	default:
 		return DefWindowProc(hWnd, Msg, wParam, lParam);
 	}
